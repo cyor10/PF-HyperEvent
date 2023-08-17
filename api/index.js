26865e81843f818dc401c26d460f3c530123d28f
cyor10 <cyor.91@gmail.com>
2023-08-17 01:38:17 -0500

    feat(api): deploy server

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
+const env = process.env.ENV_DEPLOY || 'local';
+
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
-    console.log('Server listening at 3001'); // eslint-disable-line no-console
+    if (env === 'local') {
+      console.log('Server listening locally at 3001');
+    } else {
+      console.log('Server listening on hosting');
+    }
   });
 });
--- api/src/db.js
+++ api/src/db.js
@@ -3,13 +3,22 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY,
 } = process.env;
+const env = process.env.ENV_DEPLOY || 'local';
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/hyperevents`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+if (env === 'local') {
+  sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/hyperevents`, {
+    logging: false,
+    native: false,
+  });
+} else {
+  sequelize = new Sequelize(DB_DEPLOY, {
+    logging: false,
+    native: false,
+  });
+};
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
