52086d700e9053f85d71ae76a01cbc8d32e4294d
Jo <jose_f_dirazar@hotmail.com>
2023-08-18 04:06:48 -0300

    como usuario puedo crear un evento

--- api/src/controllers/createEvent.js
+++ api/src/controllers/createEvent.js
@@ -1,17 +1,19 @@
-const {Events} = require('../db')
+const {Event} = require('../db')
 const cloudinary = require("../utils/cloudinaryConfig")
+const { v4: uuidv4 } = require('uuid');
 
 async function createEvent(req, res) {
+    const id = uuidv4();
     try {
-        const {id, name, stock} = req.body
+        const { price, stock, category, rating, review, eventname, orgname, place, eventdate} = req.body
         const imgFile = req.file
         const bufferString = Buffer.from(imgFile.buffer).toString('base64')
         const imgToCloud = "data:" + imgFile.mimetype + ";base64," + bufferString;
-        if(!id || !name || !stock) return res.status(404).json({error: 'Id, name and tickets is required.'})
-        const image = await cloudinary.uploader.upload(imgToCloud, { public_id: imgFile.originalname })
-        const createdEvents = await Events.create({id, name, stock, image})
+        if(!id || !price || !stock || !category || !rating || !review || !eventname || !orgname || !place || !eventdate || !imgFile) return res.status(404).json({error: 'Id, name and tickets is required.'})
+        const eventimage = await cloudinary.uploader.upload(imgToCloud, { public_id: imgFile.originalname })
+        const createdEvents = await Event.create({id, price, stock, eventimage: eventimage.url, category, rating, review, eventname, orgname, place, eventdate})
 
-        return res.json({events: createdEvents.dataValues})
+        return res.json({created: createdEvents.dataValues})
 
     } catch (error) {
         console.log(error)
