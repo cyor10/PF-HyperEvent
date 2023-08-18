9b4ea261cfe9b13f3f50671e3e717835e0b36bdd
cyor10 <cyor.91@gmail.com>
2023-08-18 01:23:36 -0500

    fix(api): update environment variable

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-const DB_PORT = process.env.DB_PORT;
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(DB_PORT, () => {
-    console.log(`Server listening at ${DB_PORT}`); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`Server listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
