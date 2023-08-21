e235850ef3216262384a40ef3859a9a334f45e9e
Douglas Rondon <dgsito1@gmail.com>
2023-08-21 14:04:56 -0400

    client/home

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+    domains: ['seatgeek.com'],
+    },
+}
 
 module.exports = nextConfig
--- client/src/app/components/Footer/Footer.js
+++ client/src/app/components/Footer/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram } from "@fortawesome/free-solid-svg-icons"
+
 
 export default function Footer() {
   return (
-    <footer>
-        <h3>Footer</h3>
+    <footer className='bg-[#000] h-14'>
+        <h3 className='text-white text-center pt-3'>Copyright © 2023</h3>
     </footer>
   )
 }
