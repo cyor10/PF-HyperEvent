3248b1fa9fc63f880c5657b3c0c2dc6a8e4bbda7
Franco Carreira <carreirafranco@gmail.com>
2023-09-01 14:58:33 -0300

    Update provider.js

--- client/src/redux/provider.js
+++ client/src/redux/provider.js
@@ -1,13 +1,13 @@
 'use client'
 import { Provider } from "react-redux"
 import { store } from './store'
-import { sessionProvider } from "next-auth/react";
+import { SessionProvider } from "next-auth/react";
 export function Providers({children}) {
     return (
-        <sessionProvider session={session}>
+        <SessionProvider session={session}>
             <Provider store={store}>
                 {children}
             </Provider>
-        </sessionProvider>
+        </SessionProvider>
     )
 }
