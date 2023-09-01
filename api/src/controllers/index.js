2ab11d851ae1fb8ac0d230d778555987fbaabbdd
Yaneth Orduz <122695457+cyor10@users.noreply.github.com>
2023-09-01 09:55:38 -0500

    feat(api): minor changes

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,4 +1,4 @@
-const { signupUser, loginUser, protectedUser,loginGoogleUser } = require('./user')
+const { signupUser, loginUser, protectedUser, loginGoogleUser } = require('./user')
 
 const getEvents = require('./event/getEvents')
 const { createEvent } = require('./event/createEvent')
@@ -16,11 +16,14 @@ const setStates = require('./location/setStates')
 const getStates = require('./location/getStatesByCounty')
 const setCities = require('./location/setCities')
 const getCities = require('./location/getCitiesByState')
+
 const { getSales, postSales} = require('./getSales')
+
 const deleteComment = require('./comment/deleteComment')
 const getComments = require('./comment/getComments')
 const editComment = require('./comment/patchComment')
 const postComment = require('./comment/postComment')
+
 const deleteReply = require('./replys/deleteReply')
 const getReply = require('./replys/getReply')
 const editReply = require('./replys/patchReply')
@@ -49,15 +52,17 @@ module.exports = {
     getStates,
     setCities,
     getCities,
+    
     getSales,
     postSales,
+    
     deleteComment,
     getComments,
     editComment,
     postComment,
+    
     deleteReply,
     getReply,
     editReply,
     postReply
-
 }
