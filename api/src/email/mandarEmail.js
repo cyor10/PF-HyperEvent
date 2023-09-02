cef2240554966ecd9bd2500e30920b843a5ec09b
Franco Carreira <carreirafranco@gmail.com>
2023-09-02 20:28:34 -0300

    feat(mail)/sales

--- api/src/email/mandarEmail.js
+++ api/src/email/mandarEmail.js
@@ -45,4 +45,80 @@ async function sendEmail(newUser) { // Pasar los datos del usuario como argument
    
     }
 
-    module.exports = sendEmail
+    async function sendEmailPayment(newUser) { // Pasar los datos del usuario como argumento
+      const transport = nodemailer.createTransport({
+        service: "Gmail",
+        port: 465, 
+        secure: true, 
+        auth: {
+          user: "my.hyperevents@gmail.com",
+          pass: "lbgkhodyukbjxrdk",
+        },
+      });
+      let emailContent=""
+      const imageUrl ="https://cdn.discordapp.com/attachments/1137881023183593592/1142140899976032296/OIG.png";
+      if(newUser.status==="approved"){      
+        emailContent = `
+      <div style="background-color: #f2f2f2; padding: 20px;">
+        <div style="background-color: #ffffff; border-radius: 10px; padding: 20px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+          <h2 style="color: #333;">¡Gracias por tu compra!</h2>
+          <p>Hola ${newUser.name + " " + newUser.last_name},</p>
+          <p>Gracias por confiar en HYPER EVENTS.</p>
+          <p>Detalles de tu compra:</p>
+          <ul>
+            <li>Nombre de usuario: ${newUser.name + newUser.last_name}</li>
+            <li>Correo electrónico: ${newUser.email}</li>
+            <li>Total de compra: $${newUser.amount} pesos</li>
+          </ul>
+          <p>Recuerda que puedes acceder a tus tickets desde la pagina</p>
+          <p>¡Esperamos que disfrutes tu evento!</p>
+          <p>Saludos,<br>El equipo de HYPER EVENTS</p>
+          <img src="${imageUrl}" alt="Imagen de firma" style="max-width: 20%; margin-top: 20px;align-items: center;">
+        </div>
+      </div>
+    `;}
+    if(newUser.status==="failure"){
+      emailContent = `
+      <div style="background-color: #f2f2f2; padding: 20px;">
+        <div style="background-color: #ffffff; border-radius: 10px; padding: 20px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+          <h2 style="color: #333;">¡No se pudo concretar tu compra!</h2>
+          <p>Hola ${newUser.name + " " + newUser.last_name},</p>
+          <p>Lamentablemente hubo un error con tu compra.</p>
+          <p>Puedes volver a intentarlo o contactarte con el administrador</p>
+          <p>¡Esperamos que puedas disfrutar de tu evento!</p>
+          <p>Saludos,<br>El equipo de HYPER EVENTS</p>
+          <img src="${imageUrl}" alt="Imagen de firma" style="max-width: 20%; margin-top: 20px;align-items: center;">
+        </div>
+      </div>
+    `
+    }
+    if(newUser.status==="pending"){
+      emailContent = `
+      <div style="background-color: #f2f2f2; padding: 20px;">
+        <div style="background-color: #ffffff; border-radius: 10px; padding: 20px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+          <h2 style="color: #333;">¡Su compra se encuentra pendiente!</h2>
+          <p>Hola ${newUser.name + " " + newUser.last_name},</p>
+          <p>Una vez se realize su pago recibira un mail con las entradas correspondientes.</p>
+          <p>Muchas gracias por confiar en HYPER EVENTS</p>
+          <p>¡Esperamos que puedas disfrutar de tu evento!</p>
+          <p>Saludos,<br>El equipo de HYPER EVENTS</p>
+          <img src="${imageUrl}" alt="Imagen de firma" style="max-width: 20%; margin-top: 20px; align-items: center;">
+        </div>
+      </div>
+    `
+    }
+
+        try {
+            await transport.sendMail( {
+              from: "my.hyperevents@gmail.com",
+              to: newUser.email,
+              subject: "¡Compras en HyperEvents!",
+              html: emailContent,
+  
+                  });
+                  console.log('Correo electrónico enviado correctamente.');
+                } catch (error) {
+                  console.error('Error al enviar el correo electrónico:', error);
+                };        
+      }
+    module.exports = { sendEmail , sendEmailPayment }
--- client/src/app/payment/pending/page.js
+++ client/src/app/payment/pending/page.js
@@ -1,15 +1,40 @@
 'use client'
 import React from 'react'
 import { useSearchParams } from 'next/navigation'
-
+import { useEffect } from 'react'
+import axiosInstance from "@/utils/axiosInstance"
 export default function Pending() {
   const paymentQuery = useSearchParams()
   const status = paymentQuery.get('status')
   const paymentId = paymentQuery.get('payment_id')
+  useEffect(() => {
+    async function sendPaymentInfo() {
+      if(status==="pending"){ 
+      try {
+        const paymentInfo = JSON.parse(localStorage.getItem('paymentInfo'));
+        paymentInfo.status="pending"
+        const response = await axiosInstance.post('/sales', { paymentInfo }, {
+          headers: {
+            'Content-Type': 'application/json', // Use application/json for JSON data
+          },
+        });
 
+        if (response.status === 201) {
+          console.log('Payment information sent successfully');
+          localStorage.removeItem('paymentInfo');
+        } else {
+          console.error('Failed to send payment information');
+        }
+      } catch (error) {
+        console.error('Error sending payment information:', error);
+      }
+    }}
+
+    sendPaymentInfo();
+  }, []);
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-blue-200"> {/* Cambiar el color de fondo */}
-    <h1 className="text-3xl font-semibold mb-4 text-green-600">Pendding payment</h1> {/* Cambiar el color de texto */}
+    <h1 className="text-3xl font-semibold mb-4 text-green-600">Pending payment</h1> {/* Cambiar el color de texto */}
     <div className="bg-white p-6 rounded-lg shadow-md">
       <h2 className="text-lg font-medium mb-2 text-gray-700">Status: {status}</h2> {/* Cambiar el color de texto */}
       <h2 className="text-lg font-medium text-purple-600">Payment ID: {paymentId}</h2> {/* Cambiar el color de texto */}
